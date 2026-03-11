e24bbcb1e25fefb8edd4c7585ef193cc7d3aa33f
Heng Hor <ehor@paragoniu.edu.kh>
2026-03-11 15:25:24 +0700

    added easter eggies

--- Example World/UI/StartUI.gd
+++ Example World/UI/StartUI.gd
@@ -2,17 +2,15 @@ extends Control
 
 @onready var main_menu = $MainMenu
 @onready var how_to_play_menu = $HowToPlayMenu
+@onready var easter_egg_menu = $EasterEgg
 
-# --- NEW: Potato Background Nodes ---
+# Background/Extra Nodes
 @onready var potato_bg_1 = $PotatoBorder_Background
 @onready var potato_bg_2 = $PotatoBorder_Background2
 @onready var potato_bg_3 = $PotatoBorder_Background3
+@onready var easter_egg_button = $EasterEggButton
 
-# Button paths
-@onready var start_button = $MainMenu/VBoxContainer/Start_Button
-@onready var exit_button = $MainMenu/VBoxContainer/Exit_Button2 
-
-# Audio Nodes
+# Audio
 @onready var start_sound = $Start
 @onready var exit_sound = $Exit
 @onready var background_music = $Background
@@ -23,41 +21,49 @@ func _ready():
 	if background_music:
 		background_music.play()
 	
-	if main_menu: main_menu.show()
-	if how_to_play_menu: how_to_play_menu.hide()
+	main_menu.show()
+	how_to_play_menu.hide()
+	easter_egg_menu.hide()
+	_toggle_extras(true) # Make sure potatoes are visible at start
 
-# --- HOW TO PLAY SWAP ---
+# --- HELPER: Shows/Hides the potatoes and the floating egg button ---
+func _toggle_extras(is_visible: bool):
+	if potato_bg_1: potato_bg_1.visible = is_visible
+	if potato_bg_2: potato_bg_2.visible = is_visible
+	if potato_bg_3: potato_bg_3.visible = is_visible
+	if easter_egg_button: easter_egg_button.visible = is_visible
+
+# --- MENU NAVIGATION ---
 
 func _on_how_to_play_pressed():
 	if is_transitioning: return
-	
-	if main_menu and how_to_play_menu:
-		main_menu.hide()
-		how_to_play_menu.show()
-		
-		# Hide the potatoes when the orange screen opens!
-		if potato_bg_1: potato_bg_1.hide()
-		if potato_bg_2: potato_bg_2.hide()
-		if potato_bg_3: potato_bg_3.hide()
+	main_menu.hide()
+	how_to_play_menu.show()
+	_toggle_extras(false) # Hide the fluff
 
 func _on_back_button_pressed():
 	if is_transitioning: return
-	
-	if main_menu and how_to_play_menu:
-		how_to_play_menu.hide()
-		main_menu.show()
-		
-		# Bring the potatoes back when returning to the main menu!
-		if potato_bg_1: potato_bg_1.show()
-		if potato_bg_2: potato_bg_2.show()
-		if potato_bg_3: potato_bg_3.show()
+	how_to_play_menu.hide()
+	main_menu.show()
+	_toggle_extras(true) # Bring fluff back
 
-# --- ORIGINAL START/EXIT LOGIC ---
+func _on_easter_egg_button_pressed():
+	if is_transitioning: return
+	main_menu.hide()
+	easter_egg_menu.show()
+	_toggle_extras(false) # Hide the fluff
+
+func _on_easter_egg_back_pressed():
+	if is_transitioning: return
+	easter_egg_menu.hide()
+	main_menu.show()
+	_toggle_extras(true) # Bring fluff back
+
+# --- GAME ACTIONS ---
 
 func _on_start_button_pressed():
 	if is_transitioning: return
 	is_transitioning = true
-	
 	background_music.stop()
 	start_sound.play()
 	await start_sound.finished
@@ -66,7 +72,6 @@ func _on_start_button_pressed():
 func _on_exit_button_pressed():
 	if is_transitioning: return
 	is_transitioning = true
-	
 	background_music.stop()
 	exit_sound.play()
 	await exit_sound.finished
